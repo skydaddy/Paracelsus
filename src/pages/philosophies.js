0ff4ef384ae3cea13dd6063e1db53da49316a13a
Andres Hevia <eternal.aj@gmail.com>
2020-02-07 14:20:56 +0100

    Added text.

--- src/pages/philosophies.js
+++ src/pages/philosophies.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function () {
   return (
     <div className="content">
-      <p>Let us discover a new world within the reflections of those who lived before us</p>
+      <p>Let us discover a new world within the reflections of those who lived before us.</p>
+      <p>Never give up my friend, no matter what</p>
     </div>
   )
 }
