1d92a90ad3a0ac5eda4868b355eebf095d69d46e
Andres Hevia <eternal.aj@gmail.com>
2020-03-02 11:22:13 +0100

    Updated packages and playing with spring

--- src/pages/philosophies.js
+++ src/pages/philosophies.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 export default function () {
+  const props = useSpring({ opacity:1, from: { opacity: 0 } });
   return (
-    <div className="content">
+    <div className="content" id="wow">
+      <animated.div style={props}>
       <p>Let us discover a new world within the reflections of those who lived before us.</p>
       <p>Never give up my friend, no matter what</p>
+      </animated.div>
     </div>
   )
 }
